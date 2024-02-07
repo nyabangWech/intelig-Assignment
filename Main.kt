79ebd23899f618410015932698c1687d0fd576d5
nyabangWech <nyabangwech@gmail.com>
2024-02-07 09:08:11 +0300

    file

---
+++ Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var fullName = "Nyabang Wech"
+    println(fullName)
+    var age = 20
+    println(age)
+    var phoneNumber = 101669313
+    println(phoneNumber)
+var weight = 54.5
+    println(weight)
+  var citizen ="southsudan"
+    println(citizen)
+
+}
